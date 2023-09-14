cf28ae06eb15e34091fbb64449b0e01f4408c763
SEAN-59 <ksg3452@gmail.com>
2023-09-14 13:18:19 +0900

    Initial Commit

---
+++ StudyProject/App/CommonConstants.swift
@@ -0,0 +1,11 @@
+//
+//  CommonConstants.swift
+//  StudyProject
+//
+//  Created by TAnine on 2023/09/14.
+//
+
+import UIKit
+
+let mainNavigationController = UINavigationController()
+
---
+++ StudyProject/App/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  StudyProject
+//
+//  Created by TAnine on 2023/09/14.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
